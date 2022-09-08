d651f9b9ac85db4801991633e62e0287125efd26
kalz2q <katsu95i@gmail.com>
2022-09-09 07:52:31 +0900

    mail

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React ,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-css-before-after"
+  })
   return (
     <div className="App">
-      useEffect(()=> {
-        document.title = "react-css-before-after"
-      })
+      <div className="content-after">ok?</div>
+      <a href="YOUR_URL" className="contact-btn">お問い合わせはこちら</a>
     </div>
   );
 }
